a1f69a0bbadf15f35bac6101d6596947a73b1570
Dominika <dominika.luczynska83@gmail.com>
2023-12-13 09:11:16 +0100

    form done

--- src/jsx/components/contakt.jsx
+++ src/jsx/components/contakt.jsx
@@ -1,17 +1,44 @@
 function Contact() {
+
     return (
         <>
 
             <div className="contact__inner">
                 <div className="contact__content">
-                    <div className="contact__hdl"><h2>O nas</h2></div>
-                    <div className="contact__ornament"><p>&nbsp;</p></div>
-                    <div className="contact_text"><p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p></div>
-                    <div className="contact__sign img"></div>
-                </div>
-                <div className="contct__img"></div>
-            </div>
 
+                    <div className="contact__hdl"><h2>Skontaktuj się z nami</h2></div>
+                    <div className="contact__ornament"><p>&nbsp;</p></div>
+
+                    <form className="contact__form">
+                        <div className="line">
+                            <div className="input__box">
+                                <div className="input__row brake">
+                                    <label htmlFor="name">Wpisz swoje imię</label>
+                                    <input type="text" placeholder="Imię" id="name"/>
+                                </div>
+                            </div>
+
+                            <div className="input__box">
+                                <div className="input__row">
+                                    <label htmlFor="surname">Wpisz swój email</label>
+                                    <input type="text" placeholder="Nazwisko" id="surname"/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="textarea__box">
+                            <label htmlFor="textareaame" style={{fontSize: '12px', fontWeight: '900',}}>Wpisz swoją wiadomość</label>
+                            <textarea rows="5" style={{width: '528px', resize: 'none', marginTop: '8px', backgroundColor: 'transparent', outlineStyle: 'none', borderTop: 'none', borderLeft: 'none', borderRight: 'none'
+                            }}  placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." id="textareaame"></textarea>
+                        </div>
+
+                        <button className="button-sub">Wyślij</button>
+                    </form>
+
+                </div>
+                <div className="contact__img"></div>
+
+            </div>
         </>
     );
 }
---
+++ src/jsx/components/social-media-bar.jsx
@@ -0,0 +1,14 @@
+function SocialMediaBar() {
+    return (
+        <>
+            <div className="social__media">
+                {/*<div className="cc"><p>Copyright by Coders Lab</p></div>*/}
+                <div className="icon__media"></div>
+                <div className="icon__media"></div>
+            </div>
+
+        </>
+    );
+}
+
+export default SocialMediaBar;
